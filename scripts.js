a0a93e211f2edec9b3fd034a0c1e2ca95630a94c
Icey67 <ereitenbach67@gmail.com>
2024-09-14 13:12:00 +0200

    BIG UPDATE, FINALLY REALLY FUNCTIONAL

--- scripts.js
+++ scripts.js
@@ -3,55 +3,72 @@ document.addEventListener('DOMContentLoaded', function() {
     openTab(null, 'info');
     //stuff for randomized footer
     var messages = [
-        { text: "note: near a tree by a river, there's a hole in the ground..." },
-        { text: "note: :3" },
-        { text: "note: yeah i know that the github button isn't aligned, i'll fix it later. update: fixed." },
-        { text: "tip: did you know? refreshing this site changes this text." },
-        { text: "tip: don't mine at night" },
-        { text: "note: well here we are again, it's always such a pleasure..." },
-        { text: "note: huge success" },
-        { text: "note: i'd just like to interject for a moment...", url: "/extras/stallman.html" },
-        { text: "note: sometimes it's ok to just guhhhhh" },
+        { text: "i67's note 1: images are broken, i think." },
+        { text: "AHAHHAHahHAHHahAHAHHHAAHAHAHHAAHHAHHAHH" },
+        { text: "what is going on here?" },
+        { text: "pro tip: refreshing this site changes this text. and maybe some other things too..." },
+        { text: "pro tip: don't mine at night", url: "https://youtu.be/X_XGxzMrq04?si=20m4vGQfSjLz-gug" },
+        { text: "well here we are again, it's always such a pleasure", url: "https://youtu.be/dVVZaZ8yO6o?si=CHhGSqs-l8mkj1UC" },
+        { text: "someone made a note here: 'HUGE SUCCESS'", url: "https://youtu.be/SXRteMSSZ14?si=Xc8FZXwOpcVi-cHi" },
+        { text: "i canNOT hold it in anymore, i just wanna- -you know?", url: "https://youtu.be/gixxZZOkBEA?si=viPryMoCR6jurSgy" },
         { text: "oobe says: please just log in with a microsoft account i swear we won't track you come on please!!11!1!" },
         { text: "oobe says: what do you mean you have no wifi drivers installed? you need them!" },
         { text: "oobe says: use our shit!" },
         { text: "oobe says: even better, use an online account!" },
         { text: "oobe says: please allow us to auto-import your data into our shitty browser we shove down your throat!" },
         { text: "oobe hates: ~$ oobe\\bypassnro" },
-        { text: "note: in soviet russia, arch installs you." },
-        { text: "note: ¯\\_(ツ)_/¯" },
+        { text: "someone's note: 'in soviet russia, website codes you.'" },
+        { text: "¯\\_(ツ)_/¯" },
         { text: "oobe says: no, we won't give you an option to just skip the m$ account!!" },
+        { text: "DISTRACTION!", url: "https://youtu.be/ZhFVt5uPdW0?si=zp1mZq--J5zImJ4B" },
+        { text: "i67's note 2: when clicking links in this website, you better hold the middle mouse button, or else."},
+        { text: "i67's update to note 1: the logo works, but the other images don't"},
+        { text: "i67's second update to note 1: the images do work, just forgor to add files tho 💀"},
+        { text: "i67's update to note 2: you don't need to do that anymore, links open in new tabs"},
+        { text: "i67's note 3: lore coming soon(trademark)" },
+        { text: "i67's update to note 3: trademark doesn't work properly in my brand font, it just looks like shit: ™"},
+        { text: "micro$oft says: michaelsoft binbows is not real", url: "https://youtu.be/_xEMG_tt1Vc?si=tYjs93HMIC0UTvgw" },
+        { text: "micro$oft says: we totally don't force you to buy a windows license, even though you can technically use it for free. wait that's illegal." },
+        { text: "micro$oft says: read our terms and service agreement before you use windows!", url: "/extras/microsoftlicensing.html" },
+        { text: "do you even read these?" },
+        { text: "micro$oft says: if you don't buy our product, you are our product.²"},
+        { text: "micro$oft replies to ²: THAT IS TOTALLY NOT OUR MOTTO!"},
+        { text: "hello everybody my name is welcome- what?", url: "https://youtu.be/Ebuu7FhX4tc?si=inD6Jui89T15vz0Y" },
+        { text: "yo i just got these new steel toed boots", url: "https://youtu.be/7QeLjy65OHY?si=VJyt4LZJgqAfukNi" },
+        { text: "my name is kira yoshikage", url: "/extras/kirayoshikage.html" },
+        { text: "i67's note 4: kira's speech is looped even though i set looping to false 😢" },
+        { text: "DISTRACTION!", url:"https://youtu.be/XP_ZivuN6iY?si=sgEJOPF-iHD0C_Vf" },
+        { text: "he turned himself into an angry sausage", url:"https://youtu.be/qaUSuh1ICJw?si=HkLq7H2Y1tJFVufn"},
+        {text:"i67's update to note 4: nevermind i fixed it."},
+        {text:"i67's note 5: i think mobile view is broken"}
     ];
 
     var randomMessage = messages[Math.floor(Math.random() * messages.length)];
 
     var footer = document.getElementById('footer');
-    footer.innerHTML = '<input class="noteMS" type="image" height="30" width="30" src="/media/music.png" name="B1" onclick="randomlink()"><span class="music-note"></span>';
+    footer.innerHTML = '<div class="githublogo"><input type="image" src="/media/github-mark-white.svg" alt="githubLogo" height="30" width="30" onclick="window.open(\'https://www.github.com/icey67\', \'_blank\')"></div>';
 
     if (randomMessage.url) {
         var link = document.createElement('a');
         link.href = randomMessage.url;
         link.textContent = randomMessage.text;
         link.style.color = "white"; // Change the color if needed
+        link.target = "_blank"; //makes link open in new tab hopefully
         footer.appendChild(link);
     } else {
         footer.appendChild(document.createTextNode(randomMessage.text));
     }
+    //funny switcheroo logo
+    var mainLogo = '/media/IceyBanner.png'; //og logo
+    var altLogo = '/media/IC67.png'; //lore
+    var randomNumber = Math.floor(Math.random() * 4); //randomly choose between 0-3 (25%)
+
+    if (randomNumber === 0) {                           //it hits the gold mine
+        document.getElementById('logo').src = altLogo;  //it's in
+    }
 });
-var geometryDash = new Audio('/media/gd.mp3');
 var randomlinks = [];
-randomlinks[0] = "https://www.youtube.com/watch?v=yGZr98GEs0U";
-randomlinks[1] = "https://www.youtube.com/watch?v=AS58aeJQI4Y";
-randomlinks[2] = "https://www.youtube.com/watch?v=cygu65ytwTc";
-randomlinks[3] = "https://www.youtube.com/watch?v=M9J6DKJXoKk";
-randomlinks[4] = "https://www.youtube.com/watch?v=20dIl2fl5GM";
-randomlinks[5] = "https://www.youtube.com/watch?v=BwUBkKKP27A";
-randomlinks[6] = "/media/gd.mp3";
-randomlinks[7] = "https://www.youtube.com/watch?v=5kca9KVKy04";
-randomlinks[8] = "https://www.youtube.com/watch?v=utP11PQEsZw";
-randomlinks[9] = "https://www.youtube.com/watch?v=5BZLz21ZS_Y";
-randomlinks[10] = "https://www.youtube.com/watch?v=Si8PyULlt7Q";
-randomlinks[11] = "https://www.youtube.com/watch?v=FmW6RNT2jCs";
+randomlinks[0] = ""; //gotta add my own links here
 
 function randomlink() {
     console.log("randomizing link for music...")
